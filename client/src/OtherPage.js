f5fb6db9cd3e7fd20c504bc3c4841a7e2fb13caf
Matthew Carr <matthewjhcarr@gmail.com>
2021-10-12 17:33:52 +0100

    Added readme and updated text

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 export default () => {
   return (
     <>
-      Dolkieeeee! Woohooooooo! 
-      <Link to="/">Go home!</Link>
+      Dolkieeeee! Woohooooooo!<br />
+      <Link to="/">Wanna look at numbers again?</Link>
     </>
   )
 }
