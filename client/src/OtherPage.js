fe006a95d0f4dd94c4a996b44edeed36d97875ac
Matthew Carr <matthewjhcarr@gmail.com>
2021-10-13 09:13:30 +0100

    Updated frontend

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {catthew} from '../img/catthew.jpeg'
 
 export default () => {
   return (
     <>
-      Dolkieeeee! Woohooooooo! 
+      <image src={catthew} alt="catthew" />
+      <br />
       <Link to="/">Go home!</Link>
     </>
   )
